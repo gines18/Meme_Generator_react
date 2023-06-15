e3ea5ee5d6059d52bc8a659335d501f71499c219
gines18 <ruczkowski.m@gmail.com>
2023-06-15 15:35:27 +0100

    add Meme component

---
+++ Meme_generator/src/components/Meme.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Meme.css'
+
+function Meme() {
+  return (
+<>
+    <div className="meme-container"> 
+
+<div className="input-meme">
+<input type="text" />
+    <input type="text" />
+</div>
+
+<div className="button">
+        <button>Get a new meme image</button>
+    </div>
+   
+
+    </div>
+    </>
+  
+  )
+}
+
+export default Meme
